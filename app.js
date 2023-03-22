8cc0f79fb75e11e4a93192528579821645a52087
Lucas-Cold <lucas.coldreturn@gmail.com>
2023-03-22 12:34:22 -0300

    localiza ver1.0

---
+++ app.js
@@ -0,0 +1,28 @@
+const express = require('express')
+const database = require('./configs/database')
+
+// conexão com o banco de dados
+database.mongoose
+    .connect(database.url, {
+        useNewUrlParser: true,
+        useUnifiedTopology: true
+    }).then(() => {
+        console.log('conexão estabelecida com o banco de dados.'); 
+    }).catch(error => {
+        console.log('Não foi possivel conectar com o banco de dados.', error);
+        process.exit();
+    })
+
+// criando uma aplicação express    
+const app = express();   
+
+// definindo que vamos enviar e receber JSON na requisição
+app.use(express.json());
+app.use(express.urlencoded({extended: true}));
+
+const PORT = 3000;
+const HOST = '0.0.0.0';
+
+app.listen(PORT, HOST, () => {
+    console.log(`Servidor executando na porta ${PORT}`);
+});
